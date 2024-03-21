b9d547252ebe5a51479615dac0f6faefc1ce3eed
AlvaroJTorres <mangetrone@gmail.com>
2024-03-21 18:02:47 -0500

    Add a searchbar component to filter dynamically by name

--- src/pages/TouristicAttractions.jsx
+++ src/pages/TouristicAttractions.jsx
@@ -1,23 +1,37 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { getTouristicAttractions } from "../services/colombia_api_service";
+import SearchBar from "../components/SearchBar";
 
 export default function TouristicAttractions() {
   const navigate = useNavigate()
   const [touristicAttractions, setTouristicAttractions] = useState([])
+  const [searchItem, setSearchItem] = useState('')
+  const [filteredAttractions, setFilteredAttractions] = useState([])
 
   useEffect(() => {
     const showTouristicAttractions = async () => {
       const data = await getTouristicAttractions()
       setTouristicAttractions(data)
+      setFilteredAttractions(data)
     }
     showTouristicAttractions()
   }, [])
 
+  const handleInputChange = (e) => {
+    setSearchItem(e.target.value)
+  }
+
+  useEffect(() => {
+    setFilteredAttractions(touristicAttractions.filter((attraction) => attraction.name.toLowerCase().includes(searchItem.toLowerCase())))
+  }, [searchItem])
+
   return (
     <>
       <h1>Touristic Attractions</h1>
-      {touristicAttractions.map((attraction) => (
+      <SearchBar searchItem={searchItem} handleInputChange={handleInputChange}/>
+      {filteredAttractions.length == 0 ? <p>No attractions found</p> : 
+      filteredAttractions.map((attraction) => (
         <div key={attraction.id}>
           <h3>{attraction.name}</h3>
           <p>{attraction.description}</p>

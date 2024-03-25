3c4e7dbcf14345be2f4d2a0c19d794625d30e466
jokee <qjoel44@yahoo.com>
2024-03-25 15:51:27 -0500

    Stylize SearchBar component

--- src/pages/TouristicAttractions.jsx
+++ src/pages/TouristicAttractions.jsx
@@ -36,11 +36,14 @@ export default function TouristicAttractions() {
       <h1 className="col-span-full text-3xl font-bold tracking-wide ">
         Touristic Attractions
       </h1>
+      <br />
       <div className="mt-4 mb-4">
         <SearchBar
           searchItem={searchItem}
           handleInputChange={handleInputChange}
         />
+        <br />
+        <br />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         {filteredAttractions.length === 0 ? (
@@ -49,7 +52,7 @@ export default function TouristicAttractions() {
           filteredAttractions.map((attraction) => (
             <div
               key={attraction.id}
-              className=" border bg-slate-100 bg-opacity-60 backdrop-blur-sm rounded-lg p-5 shadow-lg transition-all ease-in-out delay-100 hover:scale-125 hover:z-10 "
+              className=" border bg-slate-100 bg-opacity-60 backdrop-blur-sm rounded-lg p-5 shadow-lg transition-all ease-in-out delay-100 duration-300 hover:scale-110 hover:z-10"
             >
               <h3 className="text-xl font-semibold">{attraction.name}</h3>
               <p className="mt-2 mb-4 truncate-lines">
@@ -60,9 +63,10 @@ export default function TouristicAttractions() {
                 alt={attraction.name}
                 className="rounded-lg w-auto h-auto"
               />
+
               <Link
                 to={`${attraction.id}`}
-                className="mt-2 block text-blue-600 hover:underline"
+                className="mt-2 block text-cyan-500 hover:font-bold bottom-0"
               >
                 See more...
               </Link>

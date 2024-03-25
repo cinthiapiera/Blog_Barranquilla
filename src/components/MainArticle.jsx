ec276ccda402d096d749e6d508346e7294807c6a
Cinthiapiera <cinthia.maldonado@upch.pe>
2024-03-24 23:55:17 -0500

    create new container

--- src/components/MainArticle.jsx
+++ src/components/MainArticle.jsx
@@ -4,7 +4,7 @@ import imageDesktop from '../assets/images/image-web-3-desktop.jpg'
 
 export const MainArticle = () => {
   return (
-    <section>
+    <section className="mb-12">
       <picture>
         <source media="(max-width: 640px)" srcSet={imageMobile}/>
         <source media="(min-width: 641px)" srcSet={imageDesktop}/>
@@ -12,11 +12,11 @@ export const MainArticle = () => {
       </picture>
       <div className='sm:flex'>
         <div className="flex-1 py-6"> 
-          <h2 className="text-[40px] font-bold sm:text-[58px] leading-none">Conoce nuestros lugares turisticos</h2> 
+          <h2 className="text-[40px] font-bold sm:text-[58px] leading-none">Nuestros lugares turisticos</h2> 
         </div>
         <div className="flex-1 pt-9">
           <p className="text-[13px] mb-10 sm:text-[15px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae sunt officia quisquam culpa ducimus ad vel at neque minus doloremque, natus illo quia, eveniet modi delectus praesentium sint excepturi accusamus.</p>
-          <Link to={"TouristicAttractions"} className="flex items-center justify-center bg-blue-700 w-48 h-12 text-white hover:bg-blue-800">Touristic Attractions</Link>
+          <Link to={"TouristicAttractions"} className="flex items-center justify-center bg-slate-950 w-48 h-12 text-white hover:bg-blue-800">Touristic Attractions</Link>
         </div>
       </div>
     </section>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 // import { Link } from "react-router-dom";
 import { Header } from "../components/Header";
 import { MainArticle } from "../components/MainArticle";
+import { NewContainer } from "../components/NewContainer";
 
 export default function Home() {
   return (
     <main className="px-4 pt-6">
       <Header />
-      <MainArticle />
-      {/* <Link to={"TouristicAttractions"}>Touristic Attractions</Link> */}
+      <div className="sm:flex gap-8">
+        <MainArticle />
+        <NewContainer />
+      </div>
     </main>
   )
 }

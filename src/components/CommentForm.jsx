09bb230b126c6cdccc21152f4ba54449feec9d35
Cinthiapiera <cinthia.maldonado@upch.pe>
2024-08-25 00:05:24 -0500

    update style commmentform

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -5,13 +5,13 @@ export default function CommentForm({text, setText, saveComment}){
         <h1 className="py-6 sm:text-3xl text-2xl font-medium title-font text-gray-900">Dejanos tu comentario</h1>
       </div>
       <div className="flex lg:w-2/3 w-full sm:flex-col flex-col mx-auto px-8 sm:space-x-0 sm:space-y-4 space-y-4 sm:px-0 items-center">
+        {/* <div className="w-full">
+          <label htmlFor="alias" className="block text-sm text-gray-600">Nombre o Alias</label>
+          <input type="text" id="alias" name="alias" placeholder='Ingresa tu nombre o alias...' className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-2 px-4 leading-8 transition-colors duration-200 ease-in-out" />
+        </div> */}
         <div className="w-full">
-          <label htmlFor="full-name" className="block text-sm text-gray-600">Usuario</label>
-          <input type="text" id="full-name" name="full-name" placeholder='Escribe aquí...' className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-2 px-4 leading-8 transition-colors duration-200 ease-in-out" />
-        </div>
-        <div className="w-full">
-          <label htmlFor="email" className="block text-sm text-gray-600">Comentario</label>
-          <textarea onChange={setText} value={text} id="email" name="email" placeholder='Escribe aquí...' className="w-full h-32 bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-2 px-4 leading-6 transition-colors duration-200 ease-in-out"></textarea>
+          <label htmlFor="comment" className="block text-sm text-gray-600">Comentario</label>
+          <textarea onChange={setText} value={text} id="comment" name="comment" placeholder='Escribe aquí tu comentario...' className="w-full h-32 bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-2 px-4 leading-6 transition-colors duration-200 ease-in-out"></textarea>
         </div>
         <button onClick={saveComment} className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Crear</button>
       </div>
